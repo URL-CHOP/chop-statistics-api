0bb3d538ae2af49bb90ba486def463d7ae082842
pjh374968@gmail.com <pjh374968@gmail.com>
2019-02-05 15:09:05 +0900

    feat: #2 통계, 리다이렉트 시 gRPC 서버측 구현

--- src/main/java/me/nexters/chopstatsapi/ChopStatsApiApplication.java
+++ src/main/java/me/nexters/chopstatsapi/ChopStatsApiApplication.java
@@ -1,14 +1,36 @@
 package me.nexters.chopstatsapi;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import me.nexters.chopstatsapi.grpc.StatsService;
+import me.nexters.chopstatsapi.grpc.UrlClickService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChopStatsApiApplication {
+public class ChopStatsApiApplication implements ApplicationRunner {
+
+    public static Logger logger = LoggerFactory.getLogger(ChopStatsApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ChopStatsApiApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Server server = ServerBuilder
+                .forPort(6565)
+                .addService(new UrlClickService())
+                .addService(new StatsService())
+                .build();
+
+        server.start();
+        logger.info("gRPC server running!");
+        server.awaitTermination();
+    }
 }
 

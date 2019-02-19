6c11510d66cbe6d16f86544a3aa89f6416d8b34d
JunHoPark <pjh374968@gmail.com>
2019-02-20 00:22:56 +0900

    refactor: 쿼리 수정
    디폴트 값 0에서 1로 수정

--- src/main/java/me/nexters/chopstatsapi/repository/mapper/UrlClickMapper.java
+++ src/main/java/me/nexters/chopstatsapi/repository/mapper/UrlClickMapper.java
@@ -18,22 +18,22 @@ public interface UrlClickMapper {
     void insertClickTime(String shortUrl, LocalDateTime date);
 
     @Insert("INSERT INTO platform_count(short_url, mobile, browser) " +
-            "VALUES (#{param1}, 0, 0)" +
+            "VALUES (#{param1}, 1, 0)" +
             "ON DUPLICATE KEY UPDATE mobile = mobile + 1")
     void insertMobilePlatform(String shortUrl);
 
     @Insert("INSERT INTO platform_count(short_url, mobile, browser) " +
-            "VALUES (#{param1}, 0, 0)" +
+            "VALUES (#{param1}, 0, 1)" +
             "ON DUPLICATE KEY UPDATE browser = browser + 1")
     void insertBrowserPlatform(String shortUrl);
 
     @Insert("INSERT INTO referer_count(short_url, referer, count)" +
-            "VALUES (#{param1}, #{param2}, 0)" +
+            "VALUES (#{param1}, #{param2}, 1)" +
             "ON DUPLICATE KEY UPDATE count = count + 1")
     void insertReferer(String shortUrl, String referer);
 
     @Insert("INSERT INTO total_count(short_url, total_count)" +
-            "VALUES (#{param1}, 0)" +
+            "VALUES (#{param1}, 1)" +
             "ON DUPLICATE KEY UPDATE total_count = total_count + 1")
     void insertTotalCount(String shortUrl);
 }
